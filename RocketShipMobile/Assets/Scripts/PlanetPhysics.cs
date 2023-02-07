fea16005c3dce4cae65ea61df5f3c97e9de2d6cf
SeanDLOKM <sean.dlokm@gmail.com>
2023-02-07 19:05:37 +0000

    Added: -continuous movement upon pressing space, correct orbiting of planets, collision detection, rotation recalibration

--- RocketShipMobile/Assets/Scripts/PlanetPhysics.cs
+++ RocketShipMobile/Assets/Scripts/PlanetPhysics.cs
@@ -1,3 +1,4 @@
+/*
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -33,3 +34,4 @@ public class PlanetPhysics : MonoBehaviour
     }
     
 }
+*/
--- RocketShipMobile/Assets/Scripts/PlayerInput.cs
+++ RocketShipMobile/Assets/Scripts/PlayerInput.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    [SerializeField] float playerSpeed = 2f;
+    [SerializeField] float launchSpeed = 2f;
+    [SerializeField] float orbitSpeed = 2f;
     public GameObject targetPlanet = null;
     bool moving = false;
+    bool calibratingPosition = false;
 
     void Start()
     {
@@ -15,28 +17,70 @@ public class PlayerInput : MonoBehaviour
 
     void Update()
     {
-        // orbitPlanet();
-        moveForward();
+        OrbitPlanet();
+        Launch();
     }
 
-    void moveForward()
+    void Launch()
     {
         if (Input.GetKey("space"))
         {
             targetPlanet = null;
-
-            // while (targetPlanet == null)
-            //{
-            transform.Translate(Vector3.up * Time.deltaTime * playerSpeed);
-            //}
+            if (moving == false)
+            {
+                moving = true; // Stops moving being triggered again if the object is already moving.
+                StartCoroutine(MoveForward());
+            }
         }
     }
 
-    // void orbitPlanet()
-    // {
-    //     if (!Input.GetKey("space") && targetPlanet != null)
-    //     {
+    IEnumerator MoveForward()
+    {
+        while (targetPlanet == null) // Only runs while it is not orbiting a planet.
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * launchSpeed);
+            yield return null;
+        }
+    }
 
-    //     }
-    // }
+    void OrbitPlanet()
+    {
+        if (targetPlanet != null && !calibratingPosition)
+        {
+            transform.RotateAround(targetPlanet.transform.localPosition, Vector3.back, Time.deltaTime * orbitSpeed);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (!calibratingPosition)
+        {
+            if (other.gameObject.tag == "Planet")
+            {
+                moving = false;
+                calibratingPosition = true;
+                targetPlanet = other.gameObject;
+                float targetPlanetX = targetPlanet.transform.position.x;
+                RecalibratePosition(targetPlanetX);
+            }
+        }
+    }
+
+    void RecalibratePosition(float targetX) //Used to make Ship orbit a newly collided planet correctly.
+    {
+        transform.rotation = Quaternion.identity; // Reset rotation
+        float angleToTarget = Vector2.Angle(transform.up, targetPlanet.transform.position - transform.position);
+
+        if (targetX > transform.position.x) // If Ship hits left side of planet
+        {
+            Debug.Log("Left side of planet");
+            transform.Rotate(new Vector3 (0f, 0f, 90 - angleToTarget));
+        }
+        else // If Ship hits right side of planet
+        {
+            Debug.Log("Right side of planet");
+            transform.Rotate(new Vector3 (0f, 0f, 90 + angleToTarget));
+        }
+        calibratingPosition = false; // Other processes (such as orbiting planet) can continue now.
+    }
 }
